1e5fbcdf12ca5be7bf60c218fbd1d2d26a5c2bf5
wbs <1134954328@qq.com>
2021-09-28 21:33:37 +0800

    引入 parcel，将 button 改为单文件组件

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('good-button', {
-  template: `
-    <button class='good-button'>按钮</button>
-  `,
-})
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Button from './button.vue'
+Vue.component('good-button', Button)
+
+new Vue({
+  el: '#app',
+})
