cffccdfa7f699246525a0dcea8417117735f5ce4
wbs <1134954328@qq.com>
2021-10-01 13:47:20 +0800

    创建两个连续 button ，button-group

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 Vue.component('good-button', Button)
 Vue.component('g-icon',Icon)
+Vue.component('good-button-group',ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="good-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang='scss'>
+.good-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  > .good-button {
+    border-radius: 0;
+    margin-left: -1px;
+
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius)
+    }
+
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
