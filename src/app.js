116381f4bcdb7c90d7f556a9a3dc6a73d4a79df6
wbs <1134954328@qq.com>
2021-10-01 20:35:27 +0800

    使用 chai.expect 添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -2,14 +2,83 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+
 Vue.component('good-button', Button)
-Vue.component('g-icon',Icon)
-Vue.component('good-button-group',ButtonGroup)
+Vue.component('g-icon', Icon)
+Vue.component('good-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
-  data:{
-    loading1:false,
-    loading2:true
+  data: {
+    loading1: false,
+    loading2: true
   }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+//单元测试
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+//因为判断 icon 左右位置，需要 CSS，所有需要创建一个 div ，把 button 挂载到 div
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  button.$el.remove()
+  button.$destroy()
+}
