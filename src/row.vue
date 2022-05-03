7edaec302b76d4d0a345229e23ede665e1853b35
dingtalk_xsgrpl <1134954328@qq.com>
2022-05-03 21:25:59 +0800

    添加 row 的 left center right 属性

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -10,6 +10,12 @@ export default {
   props: {
     gutter: {
       type: Number | String,
+    },
+    align: {
+      type: String,
+      validator(value) {
+        return ['top', 'center', 'right'].includes(value)
+      },
     },
   },
   data() {
@@ -29,6 +35,10 @@ export default {
         marginLeft: -gutter / 2 + 'px',
       }
     },
+    rowClass() {
+      let { align } = this
+      return align && `align-${align}`
+    },
   },
 }
 </script>
@@ -36,5 +46,14 @@ export default {
 <style lang="scss" scoped>
 .row {
   display: flex;
+  &.align-left {
+    justify-content: flex-start;
+  }
+  &.align-center {
+    justify-content: center;
+  }
+  &.align-right {
+    justify-content: flex-end;
+  }
 }
 </style>
