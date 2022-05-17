e3e492e2840e1d350585690f17502ac6c74f3af6
dingtalk_xsgrpl <1134954328@qq.com>
2022-05-17 22:52:44 +0800

    重构一下

--- src/col.vue
+++ src/col.vue
@@ -32,17 +32,31 @@ export default {
       gutter: 0,
     }
   },
+  methods: {
+    createClass(obj, str = '') {
+      if (!obj) {
+        return []
+      }
+      let array = []
+      if (obj.span) {
+        array.push(`col-${str}${obj.span}`)
+      }
+      if (obj.offset) {
+        array.push(`offset-${str}${obj.offset}`)
+      }
+      return array
+    },
+  },
   computed: {
     colClass() {
       let { span, offset, ipad, narrowPc, pc, widePc } = this
-
+      let createClass = this.createClass
       return [
-        span && `col-${span}`,
-        offset && `offset-${offset}`,
-        ...(ipad ? [`col-ipad-${ipad.span}`] : []),
-        ...(narrowPc ? [`col-narrowPc-${narrowPc.span}`] : []),
-        ...(pc ? [`col-pc-${pc.span}`] : []),
-        ...(widePc ? [`col-widePc-${widePc.span}`] : []),
+        ...createClass({ span, offset }),
+        ...createClass(ipad, 'ipad-'),
+        ...createClass(narrowPc, 'narrow-pc-'),
+        ...createClass(pc, 'pc-'),
+        ...createClass(widePc, 'wide-pc-'),
       ]
     },
     colStyle() {
